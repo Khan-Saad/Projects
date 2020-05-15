c332cdebd0e2d599717b1679f3dd061a4d5f07ea
Khan-Saad <thesaadfather@gmail.com>
2020-05-14 21:05:53 -0400

    Update apitest.py

--- APItest/apitest.py
+++ APItest/apitest.py
@@ -6,7 +6,7 @@ querystring = {"format":"html","words":"30","paragraphs":"30"}
 
 headers = {
     'x-rapidapi-host': "alexnormand-dino-ipsum.p.rapidapi.com",
-    'x-rapidapi-key': "00ea94dd21msh3d562fddd177194p1d15f5jsn63e1fb862415"
+    'x-rapidapi-key': "nope"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
